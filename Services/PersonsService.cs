899e4516fbe98c2da73299d43fa0c92700a2171e
ManfordZeng-waikato <zengchang389@gmail.com>
2025-11-03 16:28:42 +1300

    DeletePerson-Implementation

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -205,9 +205,16 @@ namespace Services
             return matchimgPerson.ToPersonResponse();
         }
 
-        public bool DeletePerson(Guid personID)
+        public bool DeletePerson(Guid? personID)
         {
-            throw new NotImplementedException();
+            if (personID == null) throw new ArgumentNullException(nameof(personID));
+
+            Person? person =
+            _persons.FirstOrDefault(temp => temp.PersonID == (personID));
+            if (person == null) return false;
+
+            _persons.Remove(person);
+            return true;
         }
     }
 }
