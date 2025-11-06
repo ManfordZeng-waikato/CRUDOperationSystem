e2a0a9b550e7397b63596a35c9f392c241296967
ManfordZeng-waikato <zengchang389@gmail.com>
2025-11-06 21:08:39 +1300

    Tag Helper for <input>

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -7,15 +7,37 @@ namespace ServiceContracts.DTO
     public class PersonAddRequest
     {
         [Required(ErrorMessage = "Person name can't be blank")]
-        public string? PersonName { get; set; }
+        public string? PersonName
+        {
+            get; set;
+        }
         [Required(ErrorMessage = "Email can't be blank")]
         [EmailAddress(ErrorMessage = "Email should be valid adress")]
-        public string? Email { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public GenderOptions? Gender { get; set; }
-        public Guid? CountryID { get; set; }
-        public string? Address { get; set; }
-        public bool ReceiveNewsLetters { get; set; }
+        public string? Email
+        {
+            get; set;
+        }
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth
+        {
+            get; set;
+        }
+        public GenderOptions? Gender
+        {
+            get; set;
+        }
+        public Guid? CountryID
+        {
+            get; set;
+        }
+        public string? Address
+        {
+            get; set;
+        }
+        public bool ReceiveNewsLetters
+        {
+            get; set;
+        }
 
         public Person ToPerson()
         {
