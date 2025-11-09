1acb5ef269494477fe5bf6afd8b03c35fd053765
ManfordZeng-waikato <zengchang389@gmail.com>
2025-11-09 21:15:22 +1300

    stored procedure with parameter

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -36,8 +36,10 @@ namespace Services
 
             Person person = personAddRequest.ToPerson();
             person.PersonID = Guid.NewGuid();
-            _db.Persons.Add(person);
-            _db.SaveChanges();
+            /* _db.Persons.Add(person);
+             _db.SaveChanges();*/
+            _db.sp_InsertPerson(person);
+
             return ConvertPersonToPersonResponse(person);
 
         }
