570795ab72a67f360d6abd9b4ae60ca465df1120
ManfordZeng-waikato <zengchang389@gmail.com>
2025-11-26 16:17:57 +1300

    Add global exception handling middleware
    Introduced `ExceptionHandlingMiddleware` to handle exceptions globally, log errors using Serilog, and return a generic 500 error response. Added an extension method for middleware registration. Updated `Program.cs` to integrate the middleware in non-development environments. Cleaned up unused and commented-out code in `Program.cs` for clarity.

--- CRUDOperationSystem/Program.cs
+++ CRUDOperationSystem/Program.cs
@@ -1,3 +1,4 @@
+using CRUDOperationSystem.Middlewares;
 using CRUDOperationSystem.StartupExtensions;
 using OfficeOpenXml;
 using Serilog;
@@ -14,30 +15,19 @@ builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, 
     .ReadFrom.Configuration(context.Configuration)
     .ReadFrom.Services(services);
 });
-/*Data Source = (localdb)\MSSQLLocalDB;
-Initial Catalog = PersonsDatabase;
-Integrated Security = True;
-Connect Timeout = 30;
-Encrypt = False;
-Trust Server Certificate=False;
-Application Intent = ReadWrite;
-Multi Subnet Failover=False*/
 
 var app = builder.Build();
-app.UseSerilogRequestLogging();
-
-//app.Logger.LogDebug("debug-message");
-//app.Logger.LogInformation("information-message");
-//app.Logger.LogWarning("warning-message");
-//app.Logger.LogError("error-message");
-//app.Logger.LogCritical("critical-message");
-
-//app.UseHttpLogging();
 
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandlingMiddleware();
+}
+
+app.UseSerilogRequestLogging();
 
 if (builder.Environment.IsEnvironment("Test") == false)
 {
